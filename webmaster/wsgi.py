ea01fa3c46ba5cb92bb9158415ca2ea4cff24414
Gilbert Khoi Tran <gilbertktran@Gilberts-MacBook-Air.local>
2023-02-13 09:46:40 -0600

    Generate superuser through migrations:

--- webmaster/wsgi.py
+++ webmaster/wsgi.py
@@ -9,13 +9,8 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 
-from django.contrib.auth.models import User
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webmaster.settings')
 
 application = get_wsgi_application()
-
-users = User.objects.all()
-if not users:
-    User.objects.create_superuser(username="judge", email="judge@tsa.com", password="judgepassword", is_active=True, is_staff=True)
