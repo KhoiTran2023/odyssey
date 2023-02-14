873fd3d9cfeaace6cd69baf719441deab45f9138
Gilbert Khoi Tran <gilbertktran@Gilberts-MacBook-Air.local>
2023-02-13 22:13:54 -0600

    Added message, changed iframe parameters

--- odyssey/migrations/0003_create_super_user.py
+++ odyssey/migrations/0003_create_super_user.py
@@ -9,14 +9,14 @@ class Migration(migrations.Migration):
     def generate_superuser(apps, schema_editor):
         from django.contrib.auth.models import User
 
-        DJANGO_SU_NAME = os.environ.get('DJANGO_SU_NAME')
-        DJANGO_SU_EMAIL = os.environ.get('DJANGO_SU_EMAIL')
-        DJANGO_SU_PASSWORD = os.environ.get('DJANGO_SU_PASSWORD')
+        #DJANGO_SU_NAME = os.environ.get('DJANGO_SU_NAME')
+        #DJANGO_SU_EMAIL = os.environ.get('DJANGO_SU_EMAIL')
+        #DJANGO_SU_PASSWORD = os.environ.get('DJANGO_SU_PASSWORD')
 
         superuser = User.objects.create_superuser(
-            username=DJANGO_SU_NAME,
-            email=DJANGO_SU_EMAIL,
-            password=DJANGO_SU_PASSWORD)
+            username="DJANGO_SU_NAME",
+            email="DJANGO_SU_EMAIL",
+            password="DJANGO_SU_PASSWORD")
 
         superuser.save()
 
