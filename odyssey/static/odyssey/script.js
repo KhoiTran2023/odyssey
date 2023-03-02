d0bf43e09516ac461a7a53161637d21e41c005bb
sugandifs <stephanie.sugandi@gmail.com>
2023-03-01 20:34:19 -0600

    navbar changes pt 2

--- odyssey/static/odyssey/script.js
+++ odyssey/static/odyssey/script.js
@@ -4,8 +4,19 @@ function navbarScroll() {
     if (prevScrollpos > currentScrollPos) {
         document.getElementById("navbar").style.top = "0";
     } else {
-        document.getElementById("navbar").style.top = "-70px";
+        document.getElementById("navbar").style.top = "-100px";
     }
     prevScrollpos = currentScrollPos;
+}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+        document.getElementById("navbar").style.backdropFilter = "blur(1rem)";
+        document.getElementById("navbar").style.background = "hsl(0 0% 100% / 0.1)";
+    } else {
+        document.getElementById("navbar").style.backdropFilter = "none";
+        document.getElementById("navbar").style.background = "none";
+
+    }
 }
 
