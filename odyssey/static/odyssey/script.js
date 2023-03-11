580bcfc36b87ab8763cacc950891907143db14cc
sugandifs <stephanie.sugandi@gmail.com>
2023-03-11 13:33:37 -0600

    revamped the home page: moved all safety info to index.html, added an faq accordion, made a testimonial carousel

--- odyssey/static/odyssey/script.js
+++ odyssey/static/odyssey/script.js
@@ -1,3 +1,4 @@
+//collapses/uncollapses navbar on scroll
 var prevScrollpos = window.pageYOffset;
 function navbarScroll() {
     var currentScrollPos = window.pageYOffset;
@@ -11,8 +12,9 @@ function navbarScroll() {
     prevScrollpos = currentScrollPos;
 }
 
+//changes transparency and color of navbar on scroll
 function scrollFunction() {
-    if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
         document.getElementById("navbar").style.backdropFilter = "blur(1rem)";
         document.getElementById("navbar").style.background = "hsl(0 0% 100% / 0.1)";
     } else {
@@ -24,14 +26,28 @@ function scrollFunction() {
 
 function toggleChat() {
     var chat = document.getElementById("chat-container");
-    if(chat.style.display == "block")
-    {
+    if (chat.style.display == "block") {
         chat.style.display = "none";
         chat.classList.toggle("animate-expand");
     }
-    else
-    {
+    else {
         chat.style.display = "block";
         chat.classList.toggle("animate-expand");
+    }
+}
+
+//makes elements fade in on scroll
+function fadeIn() {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".fadeIn");
+
+    for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i];
+        if ($(tag).position().top < pageBottom) {
+            $(tag).addClass("visible");
+        } else {
+            $(tag).removeClass("visible");
+        }
     }
 }
