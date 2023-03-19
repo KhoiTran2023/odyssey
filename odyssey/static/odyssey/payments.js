020557004b4fb30ff97029e805f913bb5d5a1320
sugandifs <stephanie.sugandi@gmail.com>
2023-03-19 12:02:45 -0500

    added a scroll down indicator on book a tour, added pictures to 02|nausica, changed create form and login styling, fixed typo on registration.html

--- odyssey/static/odyssey/payments.js
+++ odyssey/static/odyssey/payments.js
@@ -3,13 +3,11 @@ const orderForm = document.getElementById("orderForm");
 const npForm = document.getElementById("add-payment-form");
 
 function toggleSavedToPay() {
-    if(popupForm.style.display=="none")
-    {
+    if (popupForm.style.display == "none") {
         popupForm.style.display = "block";
         npForm.style.display = "none";
     }
-    else
-    {
+    else {
         popupForm.style.display = "none";
     }
 }
@@ -19,27 +17,25 @@ function submitPayment() {
     const formData = new FormData(orderForm);
 
     fetch('/register/submit', {
-    method: 'POST',
-    body: formData
+        method: 'POST',
+        body: formData
     })
-    .then(response => {
-    if (response.status === 200) {
-    } else {
-    }
-    })
-    .catch(error => {
-    console.error(error);
-    });
+        .then(response => {
+            if (response.status === 200) {
+            } else {
+            }
+        })
+        .catch(error => {
+            console.error(error);
+        });
 }
 
 function toggleAddNewPayment() {
-    if(npForm.style.display=="none")
-    {
+    if (npForm.style.display == "none") {
         npForm.style.display = "block";
         popupForm.style.display = "none";
     }
-    else
-    {
+    else {
         popupForm.style.display = "block";
         npForm.style.display = "none";
     }
@@ -49,16 +45,16 @@ function addNewPayment() {
     const formData = new FormData(npForm);
 
     fetch('/add-payment', {
-    method: 'POST',
-    body: formData
+        method: 'POST',
+        body: formData
     })
-    .then(response => {
-    if (response.status === 200) {
-        location.reload();
-    } else {
-    }
-    })
-    .catch(error => {
-    console.error(error);
-    });
+        .then(response => {
+            if (response.status === 200) {
+                location.reload();
+            } else {
+            }
+        })
+        .catch(error => {
+            console.error(error);
+        });
 }
