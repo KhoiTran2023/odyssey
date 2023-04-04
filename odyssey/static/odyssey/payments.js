5195c39b2aa1a75893c809d4804e0ab576842397
sugandifs <stephanie.sugandi@gmail.com>
2023-04-04 15:41:10 -0500

    booking confirmation done

--- odyssey/static/odyssey/payments.js
+++ odyssey/static/odyssey/payments.js
@@ -87,12 +87,14 @@ function changeDate(departDate) {
 
 document.addEventListener("DOMContentLoaded", (event) => {
     var now = new Date();
-    var month = (now.getMonth() + 1);               
+    var month = (now.getMonth() + 1);
     var day = now.getDate();
     if (month < 10) {
-        month = "0" + month;}
+        month = "0" + month;
+    }
     if (day < 10) {
-        day = "0" + day;}
+        day = "0" + day;
+    }
     var today = now.getFullYear() + '-' + month + '-' + day;
     document.getElementById("tickets-date").value = today;
 });
