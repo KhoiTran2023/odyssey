ceb36e861d3e3fc663aa59468d221e18ce3caa74
Gilbert Khoi Tran <gilbertktran@wireless-10-148-134-110.public.utexas.edu>
2023-09-21 00:09:56 -0500

    f

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ pip install -r requirements.txt
 pip install gunicorn
 if [[ $CREATE_SUPERUSER ]];
 then
-    python webmaster/manage.py createsuperuser --no-input
+    python manage.py createsuperuser --no-input
 fi
 python manage.py collectstatic --no-input
 python manage.py migrate
