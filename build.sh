76566441956336fbb18fbe0ed7c8532a25ab2a84
Gilbert Khoi Tran <gilbertktran@wireless-10-148-134-110.public.utexas.edu>
2023-09-21 00:05:00 -0500

    fixed build

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 pip install -r requirements.txt
 pip install gunicorn
 if [[ $CREATE_SUPERUSER ]];
-then
-  python world_champ_2022/manage.py createsuperuser --no-input
-fi
+then python webmaster/manage.py createsuperuserfi --no-input
 python manage.py collectstatic --no-input
 python manage.py migrate
