2e20f8e548406d05b45775a80fb8e4d27eeb2241
Gilbert Khoi Tran <gilbertktran@wireless-10-148-134-110.public.utexas.edu>
2023-09-21 00:06:51 -0500

    i messed up

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 pip install -r requirements.txt
 pip install gunicorn
 if [[ $CREATE_SUPERUSER ]];
-then python webmaster/manage.py createsuperuserfi --no-input
+then
+    python webmaster/manage.py createsuperuser --no-input
+fi
 python manage.py collectstatic --no-input
 python manage.py migrate
