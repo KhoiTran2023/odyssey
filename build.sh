51396d9f97eda9c10e0ae47ab8cadd2e566c98ab
Gilbert Khoi Tran <gilbertktran@wireless-10-148-134-110.public.utexas.edu>
2023-09-21 00:12:07 -0500

    removing build sh createsuper user

--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
 pip install -r requirements.txt
 pip install gunicorn
-if [[ $CREATE_SUPERUSER ]];
-then
-    python manage.py createsuperuser --no-input
-fi
 python manage.py collectstatic --no-input
 python manage.py migrate
